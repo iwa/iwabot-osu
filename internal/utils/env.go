6ab58c36374ba1e0d98ee77cb4a9522c3fcaf97f
iwa <iwaongit@gmail.com>
2025-08-03 23:41:48 +0200

    feat: add env import func for discord related secrets

--- internal/utils/env.go
+++ internal/utils/env.go
@@ -18,3 +18,19 @@ func ImportOsuClientInfoFromEnv() (string, string) {
 
 	return clientId, clientSecret
 }
+
+func ImportDiscordClientInfoFromEnv() (string, string) {
+	token := os.Getenv("DISCORD_TOKEN")
+	if token == "" {
+		log.Panicln("[ERROR] No client id provided. Please set the CLIENT_ID environment variable.")
+	}
+
+	devGuild := os.Getenv("DEV_GUILD_ID")
+	if devGuild == "" {
+		log.Println("[INFO] No dev guild id provided, running in public mode.")
+	} else {
+		log.Printf("[INFO] Running in DEV mode with guild ID: %s", devGuild)
+	}
+
+	return token, devGuild
+}
