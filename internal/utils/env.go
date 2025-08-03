64e7cbe0bb6b0eb0d4133ff947071d9cdc9eb777
iwa <iwaongit@gmail.com>
2025-08-03 23:41:32 +0200

    refactor: move to log module for logging

--- internal/utils/env.go
+++ internal/utils/env.go
@@ -1,16 +1,19 @@
 package utils
 
-import "os"
+import (
+	"log"
+	"os"
+)
 
-func ImportClientInfoFromEnv() (string, string) {
+func ImportOsuClientInfoFromEnv() (string, string) {
 	clientId := os.Getenv("CLIENT_ID")
 	if clientId == "" {
-		panic("[ERROR] No client id provided. Please set the CLIENT_ID environment variable.")
+		log.Panicln("[ERROR] No client id provided. Please set the CLIENT_ID environment variable.")
 	}
 
 	clientSecret := os.Getenv("CLIENT_SECRET")
 	if clientSecret == "" {
-		panic("[ERROR] No client secret provided. Please set the CLIENT_SECRET environment variable.")
+		log.Panicln("[ERROR] No client secret provided. Please set the CLIENT_SECRET environment variable.")
 	}
 
 	return clientId, clientSecret
