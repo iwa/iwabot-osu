a0f19bf43d0ff4c14c208ca49b2fc1719cd23774
iwa <iwaongit@gmail.com>
2025-08-05 00:10:35 +0200

    feat: beautify /rs output message

--- internal/discord/handlers/interactions.go
+++ internal/discord/handlers/interactions.go
@@ -68,13 +68,38 @@ func rs(s *discordgo.Session, i *discordgo.InteractionCreate) {
 
 	var component discordgo.MessageEmbed
 	for _, score := range scores {
+		// fmt.Printf("score: %+v\n", score)
+
 		component.Title = fmt.Sprintf("%s - %s [%s] [%.2f*]", score.BeatmapSet.Artist, score.BeatmapSet.Title, score.Beatmap.Version, float64(score.Beatmap.DifficultyRating))
-		component.Description = fmt.Sprintf("%s - **%2.f** - **%2.fpp**/maxpp\n%d 300 - %d 100 - %d 50 - %dx\n%d • **%d**/%d\n`%d` • %dbpm\n%s", score.Rank, float64(score.Accuracy), float64(score.PP), score.Statistics.Count300, score.Statistics.Count100, score.Statistics.Count50, score.Statistics.CountMiss, score.TotalScore, score.MaxCombo, score.Beatmap.CountCircles+score.Beatmap.CountSliders+score.Beatmap.CountSpinners, score.Beatmap.TotalLength, score.Beatmap.BPM, strings.Join(score.Mods, " "))
-		component.Color = 0x39CC69 // Green color for success
+		component.URL = score.Beatmap.URL
+
+		component.Description = fmt.Sprintf("**%s** ・ **%1.2f%%** ・ **%1.2fpp**/maxpp\n%d <:300:1402045799164022914> %d <:100:1402045789513191566> %d <:50:1402045777324413040> %d <:X2:1402045819758317750>\n%d ・ **%dx**/%dx\n`%d:%02d` ・ %dbpm\n%s",
+			score.Rank,
+			(score.Accuracy * 100),
+			score.PP,
+			score.Statistics.Count300,
+			score.Statistics.Count100,
+			score.Statistics.Count50,
+			score.Statistics.CountMiss,
+			score.TotalScore,
+			score.MaxCombo,
+			(score.MaximumStatistics.Great + score.MaximumStatistics.LargeTickHit + score.MaximumStatistics.SliderTailHit),
+			(score.Beatmap.TotalLength / 60),
+			(score.Beatmap.TotalLength % 60),
+			score.Beatmap.BPM,
+			strings.Join(score.Mods, " "))
+
+		component.Color = 0x7b67f0
 		component.Author = &discordgo.MessageEmbedAuthor{
 			Name:    score.User.Username,
 			IconURL: score.User.AvatarURL,
 			URL:     fmt.Sprintf("https://osu.ppy.sh/users/%d", score.UserId),
+		}
+		component.Thumbnail = &discordgo.MessageEmbedThumbnail{
+			URL: score.BeatmapSet.Covers.List2x,
+		}
+		component.Footer = &discordgo.MessageEmbedFooter{
+			Text: fmt.Sprintf("Mapped by %s", score.BeatmapSet.Creator),
 		}
 	}
 
